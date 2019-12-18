4b523e68ba63a9a8d29fde0105d903d2879e1ae7
Paul Baker <paulpbkr22@gmail.com>
2019-12-18 15:25:06 -0500

    handlebars and db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Inferno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
